0c3cd65668f096ff5356f05029df009d1ea53ac2
Sukuna18 <gossfall10@gmail.com>
2023-10-02 19:18:00 +0000

    feat: :tada:

---
+++ Gestion-Pedagogique-Laravel/app/Http/Controllers/NiveauClasseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreNiveauClasseRequest;
+use App\Http\Requests\UpdateNiveauClasseRequest;
+use App\Models\NiveauClasse;
+
+class NiveauClasseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNiveauClasseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NiveauClasse $niveauClasse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(NiveauClasse $niveauClasse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNiveauClasseRequest $request, NiveauClasse $niveauClasse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NiveauClasse $niveauClasse)
+    {
+        //
+    }
+}
